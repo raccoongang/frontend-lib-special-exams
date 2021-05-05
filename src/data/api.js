a6619fc339099fbaf8ecaeb0614f370a440b193b
Sagirov Eugeniy <sagirov19@gmail.com>
2021-05-05 14:14:30 +0300

    fix: fix timeOutAttempt api

--- src/data/api.js
+++ src/data/api.js
@@ -45,7 +45,7 @@ export async function submitAttempt(attemptId) {
 }
 
 export async function timeOutAttempt(attemptId) {
-  return updateAttemptStatus(attemptId, ExamAction.TIMED_OUT);
+  return updateAttemptStatus(attemptId, ExamAction.SUBMIT);
 }
 
 export async function endExamWithFailure(attemptId, error) {
--- src/exam/Sequence.jsx
+++ src/exam/Sequence.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withExamStore } from "../hocs";
-import { ExamStatus } from '../constants';
 import { Exam } from "./Exam";
 import { getExamAttemptsData } from '../data';
 
